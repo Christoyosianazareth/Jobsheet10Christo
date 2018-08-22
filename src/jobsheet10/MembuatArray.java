e2e78864b75b47346096eb55421cfc25f8728c48
GL553VD <GL553VD@DESKTOP-I019UBP>
2018-08-22 18:42:12 +0700

    TambahInput

---
+++ src/jobsheet10/MembuatArray.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jobsheet10;
+
+/**
+ *
+ * @author GL553VD
+ */
+public class MembuatArray {
+    public static void main(String[] args){
+        //membuat array
+        String[] nama = {"LInda", "Sania", "Roni", "Rani", "Ahmad"};
+        //mengambil data array
+        
+        System.out.println(nama[2]);
+    }
+}
